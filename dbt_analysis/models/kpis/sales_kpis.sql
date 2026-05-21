9ce2ffd9a49cfefb1f4e7b305da4146f9a7fd335
ismail <user@Host-001.lan>
2026-05-21 15:29:41 +0200

    set up db done

---
+++ dbt_analysis/models/kpis/sales_kpis.sql
@@ -0,0 +1,15 @@
+{{ config(materialized='table') }}
+
+SELECT
+    month,
+    category,
+    SUM(units_sold)  AS total_units_sold,
+    SUM(revenue)     AS total_revenue,
+    AVG(revenue)     AS avg_revenue
+FROM delta.`/Users/user/Desktop/learning/reddit_etl/data/gold/sales`
+GROUP BY
+    month,
+    category
+ORDER BY
+    month,
+    category
